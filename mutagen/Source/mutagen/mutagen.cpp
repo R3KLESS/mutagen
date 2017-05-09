aaff369988457c9e837379973ff56719b82340a8
Enigma <dj-teknike@outlook.com>
2017-05-09 18:38:05 +0100

    FIRST COMMIT . had to resort out project and organise file structure . TODO login system

---
+++ mutagen/Source/mutagen/mutagen.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "mutagen.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mutagen, "mutagen" );
+ 
---
+++ mutagen/Source/mutagen/mutagenGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "mutagen.h"
+#include "mutagenGameMode.h"
+#include "mutagenCharacter.h"
+
+AmutagenGameMode::AmutagenGameMode()
+{
+
+}
